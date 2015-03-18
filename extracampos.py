71b35bef295a4044b611a31a1c722d8dbef2cdef
Pablo <elfio@hiperones.es>
2015-03-18 13:17:25 +0100

    Pequeños arreglos.

--- extracampos.py
+++ extracampos.py
@@ -62,7 +62,7 @@ def HashFile(ruta):
   #for campo in listaDatos[0].header.keys():
     #print listaDatos[0].header[campo]
 
-def JD2Date(entrada)
+def JD2Date(entrada):
   from astropy.time import Time
   dato = float(entrada)
   dia = int(dato)
@@ -71,7 +71,7 @@ def JD2Date(entrada)
   return t.iso
 
 
-def MJD2Date(entrada)
+def MJD2Date(entrada):
   from astropy.time import Time
   dato = float(entrada)
   dia = int(dato)
@@ -80,73 +80,73 @@ def MJD2Date(entrada)
   return t.iso
 
 
-def BuscaTiempo(fuente,listaCampos)
-  if DATE in listaCampos:
+def BuscaTiempo(fuente,listaCampos): # Sin acabar
+  if "DATE" in (s.rstrip(' ') for s in listaCampos):
     print "Fecha"
-  elif "DATE-AVG" or "DATE_AVG" in listaCampos:
+  elif "DATE-AVG" or "DATE_AVG" in (s.rstrip(' ') for s in listaCampos):
     print "Fecha media"
-  elif "DATE-OBS" or "DATE_OBS" in listaCampos:
+  elif "DATE-OBS" or "DATE_OBS" in (s.rstrip(' ') for s in listaCampos):
     print "Fecha obs"
-  elif "JD" or "JUL-DATE" or "JUL_DATE" in listaCampos:
+  elif "JD" or "JUL-DATE" or "JUL_DATE" in (s.rstrip(' ') for s in listaCampos):
     print "fecha juliana"
-  elif "JD_HELIO" or "JD-HELIO" in listaCampos:
+  elif "JD_HELIO" or "JD-HELIO" in (s.rstrip(' ') for s in listaCampos):
     print "fecha juliana heliocentrica"
-  elif "MJD" or "MJD-OBS" or "MJD_OBS" in listaCampos:
+  elif "MJD" or "MJD-OBS" or "MJD_OBS" in (s.rstrip(' ') for s in listaCampos):
     print "fecha juliana modificada"
-  elif "MNT_INFO" in listaCampos:
+  elif "MNT_INFO" in (s.rstrip(' ') for s in listaCampos):
     print "mnt_info"
-  elif "OPENTIME" in listaCampos:
+  elif "OPENTIME" in (s.rstrip(' ') for s in listaCampos):
     print "instante apertura obturador"
-  elif "READTIME" in listaCampos:
+  elif "READTIME" in (s.rstrip(' ') for s in listaCampos):
     print "tiempod de lectura"
-  elif "SID-TIME" or "SID_TIME" in listaCampos:
+  elif "SID-TIME" or "SID_TIME" in (s.rstrip(' ') for s in listaCampos):
     print "tiempo sideral"
-  elif "ST" or "STSTART" in listaCampos:
+  elif "ST" or "STSTART" in (s.rstrip(' ') for s in listaCampos):
     print "tiempo inicio exp"
-  elif "TIME" in listaCampos:
+  elif "TIME" in (s.rstrip(' ') for s in listaCampos):
     print "instante en el que empieza algo"
-  elif "TIME-END" or "TIME_END" in listaCampos:
+  elif "TIME-END" or "TIME_END" in (s.rstrip(' ') for s in listaCampos):
     print "instante en el que termina la última adquisición"
-  elif "TM_START" or "TM-START" in listaCampos:
+  elif "TM_START" or "TM-START" in (s.rstrip(' ') for s in listaCampos):
     print "instante de inicio indet"
-  elif "UNI-TIME" or "UNI_TIME" in listaCampos:
+  elif "UNI-TIME" or "UNI_TIME" in (s.rstrip(' ') for s in listaCampos):
     print "tiempo universal, ahí es nada"
-  elif "USEC" in listaCampos:
+  elif "USEC" in (s.rstrip(' ') for s in listaCampos):
     print "tiempo de exposición"
-  elif "UT" in listaCampos:
+  elif "UT" in (s.rstrip(' ') for s in listaCampos):
     print "inicio de algo, lo que sea"
-  elif "UTC" in listaCampos:
+  elif "UTC" in (s.rstrip(' ') for s in listaCampos):
     print "inst de inicio en segundos suma"
-  elif "UT_END" or "UT-END" in listaCampos:
+  elif "UT_END" or "UT-END" in (s.rstrip(' ') for s in listaCampos):
     print "inst de fin en segundos suma"
-  elif "UTOBS" in listaCampos:
+  elif "UTOBS" in (s.rstrip(' ') for s in listaCampos):
     print "inst aprox de inicio"
-  elif "UT_START" in listaCampos:
+  elif "UT_START" in (s.rstrip(' ') for s in listaCampos):
     print "inst de inicio indet de algo"
-  elif "CLOSTIME" in listaCampos:
+  elif "CLOSTIME" in (s.rstrip(' ') for s in listaCampos):
     print "inst de cierre del obturador"
-  elif "CTIME" in listaCampos:
+  elif "CTIME" in (s.rstrip(' ') for s in listaCampos):
     print "inst de inicio de exposicion"
-  elif "DARKTIME" in listaCampos:
+  elif "DARKTIME" in (s.rstrip(' ') for s in listaCampos):
     print "darktime. muchas cosas"
-  elif "ELAPSED" in listaCampos:
+  elif "ELAPSED" in (s.rstrip(' ') for s in listaCampos):
     print "elapsed"
-  elif "EXPOSED" in listaCampos:
+  elif "EXPOSED" in (s.rstrip(' ') for s in listaCampos):
     print "exposed"
-  elif "EXP_ID" in listaCampos:
+  elif "EXP_ID" in (s.rstrip(' ') for s in listaCampos):
     print "exp ip"
-  elif "EXPOSURE" or "EXPTIME" in listaCampos:
+  elif "EXPOSURE" or "EXPTIME" in (s.rstrip(' ') for s in listaCampos):
     print "exposure"
-  elif "EXPSTART" in listaCampos:
+  elif "EXPSTART" in (s.rstrip(' ') for s in listaCampos):
     print "expstart"
-  elif "LST" in listaCampos:
+  elif "LST" in (s.rstrip(' ') for s in listaCampos):
     print "lst"
-  elif "MNT_INFO" in listaCampos:
+  elif "MNT_INFO" in (s.rstrip(' ') for s in listaCampos):
     print "mnt info"
-  else
+  else:
     print "No se encuentra."
 
-def GetData(url)
+def GetData(url):
   fuente = pyfits.open(url)
   listaCampos = fuente[0].header.keys()
   tiempo = BuscaTiempo(fuente, listaCampos)
@@ -190,8 +190,9 @@ for (path, ficheros, archivos) in walk (directorio_imagenes):
   for file in archivos:
     if file.endswith(".fits") or file.endswith(".fit") or file.endswith(".fts"):
       ruta = path + '/' + file
-      AddCampos(ruta, archivo_nombres_campos)
+      #AddCampos(ruta, archivo_nombres_campos)
       #HashFile(ruta)
+      GetData(ruta)
       j += 1
 Sort(archivo_nombres_campos)
 msgfin = "Se han procesados " + str(j) + " archivos.", "green"
