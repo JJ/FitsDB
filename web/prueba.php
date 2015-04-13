7e83a5d5dccf202ffe610c064ae2fd8e5ea45cc1
Pablo <elfio@hiperones.es>
2015-04-13 11:50:20 +0200

    La interfaz web ya realiza consultas correctamente a la base de datos. Hay que implementar un botón de descarga para bajar los archivos que devuelve la consulta.

--- web/prueba.php
+++ web/prueba.php
@@ -1,23 +1,79 @@
 <html>
+<meta charset="utf-8" /> 
 <title>
 FitsDB v0.1.1-1
 </title>
 <link href="prueba.css" rel="stylesheet" type="text/css">
 <body>
-<form id="form1" name="form1" method="post" action="index.php" align="right">
+<H1 align=center>Interfaz web de FitsDB</H1>
+
+<form id="form1" name="form1" method="post" action="prueba.php" align="right">
   <table width="500" border="1" align="center">
   <tr>
-    <td width="200">Nombre del objeto:</td>
+    <td width="200">ID:</td>
     <td width="300">
-      <label for="nombre"></label>
-      <input type="text" value="Eris" name="nombre" id="nombre" />
-
+      <input type="text" name="idnum" id="idnum" />
     </td>
   </tr>
   <tr>
-    <td>Nick:</td>
-    <td><label for="nick"></label>
-    <input type="text" name="nick" id="nick" /></td>
+    <td>Fecha de modificación:</td>
+    <td>
+      <input type="text" name="fecha_mod" id="fecha_mod" />
+    </td>
+  </tr>
+  <tr>
+    <td>Tipo de imagen:</td>
+    <td>
+      <input type="text" name="typeimg" id="idnum" />
+    </td>
+  </tr>
+  <tr>
+    <td>Nombre del objeto:</td>
+    <td>
+      <input type="text" name="nombre_obj" id="nombre_obj" />
+    </td>
+  </tr>
+  <tr>
+    <td>Fecha de la observación:</td>
+    <td>
+      <input type="text" name="fecha_obs" id="fecha_obs" />
+    </td>
+  </tr>
+  <tr>
+    <td>Hora de la observación:</td>
+    <td>
+      <input type="text" name="tiempo_obs" id="tiempo_obs" />
+    </td>
+  </tr>
+    <tr>
+    <td>Tiempo de exposición:</td>
+    <td>
+      <input type="text" name="exptime" id="exptime" />
+    </td>
+  </tr>
+    <tr>
+    <td>Observatorio:</td>
+    <td>
+      <input type="text" name="observatorio" id="observatorio" />
+    </td>
+  </tr>
+    <tr>
+    <td>Telescopio:</td>
+    <td>
+      <input type="text" name="telescopio" id="telescopio" />
+    </td>
+  </tr>
+    <tr>
+    <td>Instrumento:</td>
+    <td>
+      <input type="text" name="instrumento" id="instrumento" />
+    </td>
+  </tr>
+    <tr>
+    <td>Filtro:</td>
+    <td>
+      <input type="text" name="filtro" id="filtro" />
+    </td>
   </tr>
   <tr>
     <td><input type="reset" value="Limpiar formulario" /></td>
@@ -28,8 +84,8 @@ FitsDB v0.1.1-1
 </table>
 </form>
 
-<H1 align=center>Resultados</H1>
-<table border=0 align=center width=1250 class='zebra'>
+<H2 align=center>Resultados</H2>
+<table border=0 align=center width=1850 class='zebra'>
   <thead>
     <tr align=center>
       <br>
@@ -41,24 +97,40 @@ FitsDB v0.1.1-1
 	Fecha Mod.
       </th>
       <br>
-      <th width=7%>
+      <th width=5%>
 	Tipo Img.
       </th>
       <br>
-      <th width=13%>
+      <th width=9%>
 	Nombre
       </th>
       <br>
-      <th width=8%>
+      <th width=5%>
 	Fecha Obs.
       </th>
       <br>
-      <th width=8%>
+      <th width=4%>
 	Tiempo Obs.
       </th>
       <br>
-      <th width=4%>
+      <th width=2%>
 	Exp.
+      </th>
+      <br>
+      <th width=9%>
+	Obs.
+      </th>
+      <br>
+      <th width=6%>
+	Tele.
+      </th>
+      <br>
+      <th width=12%>
+	Instr.
+      </th>
+      <br>
+      <th width=4%>
+	Filtro.
       </th>
       <br>
       <th width=50%>
@@ -70,20 +142,90 @@ FitsDB v0.1.1-1
 
 <?php
 //Recibir
-$idnum = strip_tags($_POST['id']);
+$idnum = strip_tags($_POST['idnum']);
 $fecha_mod = strip_tags($_POST['fecha_mod']);
-$typeimg = strip_tags($_POST['typeimp']);
+$typeimg = strip_tags($_POST['typeimg']);
 $nombre_obj= strip_tags($_POST['nombre_obj']);
 $fecha_obs= strip_tags($_POST['fecha_obs']);
 $tiempo_obs= strip_tags($_POST['tiempo_obs']);
 $exptime= strip_tags($_POST['exptime']);
+$observatorio= strip_tags($_POST['observatorio']);
+$telescopio= strip_tags($_POST['telescopio']);
+$instrumento= strip_tags($_POST['instrumento']);
+$filtro= strip_tags($_POST['filtro']);
+
+// echo $nombre_obj;
+
+// if (strlen($nombre_obj) == 0)
+// {
+//   echo "vacio";
+// }
+// else
+// {
+// echo "_" . $nombre_obj . "_";
+// }
 
 
+$prefijo = "SELECT id, moddate, imgtype, object, dateobs, timeobs, exptime, observatory, telescope, instrument, filter, rute FROM tablaobs";
+$sufijo = '';
 
+
+if (strlen($idnum) != 0) {
+  $sufijo = $sufijo . sprintf(" id like '%%%s%%'",$idnum) ." and";
+  }
+
+if (strlen($fecha_mod) != 0) {
+  $sufijo = $sufijo . sprintf(" moddate like '%%%s%%'",$fecha_mod) . " and";
+  }
+
+if (strlen($typeimg) != 0) {
+  $sufijo = $sufijo . sprintf(" typeimg like '%%%s%%'",$typeimg) . " and";
+  }
+  
+if (strlen($nombre_obj) != 0) {
+  $sufijo = $sufijo . sprintf(" object like '%%%s%%'",$nombre_obj) . " and";
+  }
+
+if (strlen($fecha_obs) != 0) {
+  $sufijo = $sufijo . sprintf(" dateobs like '%%%s%%'",$fecha_obs) . " and";
+  }
+
+if (strlen($tiempo_obs) != 0) {
+  $sufijo = $sufijo . sprintf(" timeobs like '%%%s%%'",$tiempo_obs) . " and";
+  }
+  
+if (strlen($observatorio) != 0) {
+  $sufijo = $sufijo . sprintf(" observatory like '%%%s%%'",$observatorio) . " and";
+  }
+  
+
+if (strlen($telescopio) != 0) {
+  $sufijo = $sufijo . sprintf(" telescope like '%%%s%%'",$telescopio) . " and";
+  }
+
+
+if (strlen($instrumento) != 0) {
+  $sufijo = $sufijo . sprintf(" instrument like '%%%s%%'",$instrumento) . " and";
+  }
+
+if (strlen($filtro) != 0) {
+$sufijo = $sufijo . sprintf(" filter like '%%%s%%'",$filtro);
+}
+
+if ((strlen($idnum) != 0) || (strlen($fecha_mod) != 0) || (strlen($typeimg) != 0) || (strlen($nombre_obj) != 0) || (strlen($fecha_obs) != 0) || (strlen($tiempo_obs) != 0) || (strlen($observatorio) != 0) || (strlen($telescopio) != 0) || (strlen($instrumento) != 0) || (strlen($filtro) != 0)) {
+  $montamos = $prefijo . " WHERE" . $sufijo;
+  $peticion = preg_replace('/and$/', '', $montamos);
+  }
+else {
+  $peticion = "SELECT id, moddate, imgtype, object, dateobs, timeobs, exptime, observatory, telescope, instrument, filter, rute FROM tablaobs WHERE DATE_SUB(CURDATE(), INTERVAL 30 DAY) <= moddate ORDER BY dateobs DESC";
+  }
+echo "Se ejecuta la siguiente petición a la base de datos: \n" . $peticion;
+  
+  
 
 $conexion = new mysqli("127.0.0.1", "pablo", "halconmilenario", "pruebasdb");
-$peticion = "SELECT id, moddate, imgtype, object, dateobs, timeobs, exptime, rute FROM tablaobs Where";
-$resultado = $conexion->query("SELECT id, moddate, imgtype, object, dateobs, timeobs, exptime, rute FROM tablaobs");
+
+$resultado = $conexion->query($peticion);
 $resultado -> data_seek(0);
 while ($fila = $resultado->fetch_assoc())
 {
