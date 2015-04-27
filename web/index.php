9ea02e53421be52d402f5a3f3e7984874a91be48
Pablo <elfio@hiperones.es>
2015-04-27 11:15:51 +0200

    Se han introducido varios cambios en la interfaz web. Ahora al hacer una consulta, los datos del formulario no se borran. Además de pequeñas mejoras.

--- web/index.php
+++ web/index.php
@@ -11,9 +11,26 @@ FitsDB v0.1.1-1
 //ini_set('display_errors', true);
 limpieza('descargas/');
 // limpieza('sesion/');
+
+
+//Recibir
+$idnum = strip_tags($_POST['idnum']);
+$typeimg = strip_tags($_POST['typeimg']);
+$nombre_obj= strip_tags($_POST['nombre_obj']);
+$fecha_obs1= strip_tags($_POST['fecha_obs1']);
+$fecha_obs2= strip_tags($_POST['fecha_obs2']);
+$exptime1= strip_tags($_POST['exptime1']);
+$exptime2= strip_tags($_POST['exptime2']);
+$observatorio= strip_tags($_POST['observatorio']);
+$telescopio= strip_tags($_POST['telescopio']);
+$instrumento= strip_tags($_POST['instrumento']);
+$filtro= strip_tags($_POST['filtro']);
+
+
 ?>
 
-<script language="JavaScript">
+
+<script language="JavaScript"> // Script en js para (des)marcar todas las casillas con una casilla maestras
 function cambiar(){
   var cajamaestra = document.forms['form2'].elements['marcartodos'];
   var lista = document.getElementsByClassName('A');
@@ -39,7 +56,14 @@ function cambiar(){
   <tr>
     <td>Nombre del objeto:</td>
     <td>
-	<input type="text" name="nombre_obj" list="listanombres_obj" autocomplete autofocus/>
+	<?php
+	if (strlen($nombre_obj)>0){
+	echo "<input type='text' name='nombre_obj' list='listanombres_obj' value='".$nombre_obj."' autocomplete autofocus/>";
+	}
+	else{
+	echo "<input type='text' name='nombre_obj' list='listanombres_obj' autocomplete autofocus/>";
+	}
+	?>
 	<datalist id="listanombres_obj">
 	<?php
 	$arrayobjetos = array('Varuna','Orcus','Eris');
@@ -59,17 +83,51 @@ function cambiar(){
     <tr>
     <td>Telescopio:</td>
     <td>
-      <input type="text" name="telescopio" id="telescopio" autocomplete />
+<!--       <input type="text" name="telescopio" id="telescopio" autocomplete /> -->
+	<?php
+	if (strlen($telescopio)>0){
+	echo "<input type='text' name='telescopio' id='telescopio' value='".$telescopio."' autocomplete />";
+	}
+	else{
+	echo "<input type='text' name='telescopio' id='telescopio' autocomplete />";
+	}
+	?>
     </td>
         <td>Instrumento:</td>
     <td>
-      <input type="text" name="instrumento" id="instrumento" autocomplete />
+<!--       <input type="text" name="instrumento" id="instrumento" autocomplete /> -->
+      	<?php
+	if (strlen($instrumento)>0){
+	echo "<input type='text' name='instrumento' id='instrumento' value='".$instrumento."' autocomplete />";
+	}
+	else{
+	echo "<input type='text' name='instrumento' id='instrumento' autocomplete />";
+	}
+	?>
     </td>
   </tr>
   <tr>
     <td>Fecha de la observación:</td>
     <td>
-      <input type="date" name="fecha_obs1" style="width: 140px;"></input> a <input type="date" name="fecha_obs2" style="width: 140px;"></input>
+<!--       <input type="date" name="fecha_obs1" style="width: 140px;"></input> -->
+      	<?php
+	if (strlen($fecha_obs1)>0){
+	echo "<input type='date' name='fecha_obs1' style='width: 140px' value='".$fecha_obs1."' autocomplete />";
+	}
+	else{
+	echo "<input type='date' name='fecha_obs1' style='width: 140px' autocomplete />";
+	}
+	?>
+      a 
+<!--       <input type="date" name="fecha_obs2" style="width: 140px;"></input> -->
+      	<?php
+	if (strlen($fecha_obs2)>0){
+	echo "<input type='date' name='fecha_obs2' style='width: 140px' value='".$fecha_obs2."' autocomplete />";
+	}
+	else{
+	echo "<input type='date' name='fecha_obs2' style='width: 140px' autocomplete />";
+	}
+	?>
     </td>
     <td>
     </td>
@@ -80,11 +138,27 @@ function cambiar(){
     <tr>
     <td>Filtro:</td>
     <td>
-      <input type="text" name="filtro" id="filtro" autocomplete />
+<!--       <input type="text" name="filtro" id="filtro" autocomplete /> -->
+      	<?php
+	if (strlen($filtro)>0){
+	echo "<input type='text' name='filtro' id='filtro' value='".$filtro."' autocomplete />";
+	}
+	else{
+	echo "<input type='text' name='filtro' id='filtro' autocomplete />";
+	}
+	?>
     </td>
     <td>Tipo de imagen:</td>
     <td>
-	<input type="text" name="typeimg" list="listatipos" autocomplete/>
+<!-- 	<input type="text" name="typeimg" list="listatipos" autocomplete/> -->
+      	<?php
+	if (strlen($typeimg)>0){
+	echo "<input type='text' name='typeimg' id='typeimg' list='listatipos' value='".$typeimg."' autocomplete />";
+	}
+	else{
+	echo "<input type='text' name='typeimg' id='typeimg' list='listatipos' autocomplete />";
+	}
+	?>
 	<datalist id="listatipos">
 	<?php
 	$arraytipos = array('Flat/Domme','Bias/Dark','Science');
@@ -99,7 +173,26 @@ function cambiar(){
     <tr>
     <td>Tiempo de exposición:</td>
     <td>
-      <input type="number" name="exptime1" id="exptime" step="10" style="width: 79px;" min="0"/> a <input type="number" name="exptime2" id="exptime" step="10" style="width: 79px;" min="0" />
+<!--       <input type="number" name="exptime1" id="exptime1" step="10" style="width: 79px;" min="0"/> -->
+      	<?php
+	if (strlen($exptime1)>0){
+	echo "<input type='number' name='exptime1' id='exptime1' step='10' style='width 79px;' min='0' value='".$exptime1."'/>";
+	}
+	else{
+	echo "<input type='number' name='exptime1' id='exptime1' step='10' style='width 79px;' min='0' />";
+	}
+	?>
+      a 
+<!--       <input type="number" name="exptime2" id="exptime2" step="10" style="width: 79px;" min="0" /> -->
+      	<?php
+	if (strlen($exptime2)>0){
+	echo "<input type='number' name='exptime2' id='exptime2' step='10' style='width 79px;' min='0' value='".$exptime2."'/>";
+	}
+	else{
+	echo "<input type='number' name='exptime2' id='exptime2' step='10' style='width 79px;' min='0' />";
+	}
+	?>
+
     </td>
         </td>
     <td>
@@ -112,7 +205,15 @@ function cambiar(){
     <tr>
     <td>Observatorio:</td>
     <td>
-	<input type="text" name="observatorio" list="listaobservatorio" autocomplete/>
+<!-- 	<input type="text" name="observatorio" list="listaobservatorio" autocomplete/> -->
+      	<?php
+	if (strlen($observatorio)>0){
+	echo "<input type='text' name='observatorio' id='observatorio' list='listaobservatorio' value='".$observatorio."' autocomplete />";
+	}
+	else{
+	echo "<input type='text' name='observatorio' id='observatorio' list='listaobservatorio' autocomplete />";
+	}
+	?>
 	<datalist id="listaobservatorio">
 	<?php
 	$arrayobservatorio = array('OSN','DSAZ','CGG','Teide','Atacama','IAC','la hita','lapalma');
@@ -132,7 +233,15 @@ function cambiar(){
   <tr>
     <td>ID:</td>
     <td>
-      <input type="number" name="idnum" id="idnum" />
+<!--       <input type="number" name="idnum" id="idnum" /> -->
+      	<?php
+	if (strlen($idnum)>0){
+	echo "<input type='number' name='idnum' id='idnum' min='0' value='".$idnum."' />";
+	}
+	else{
+	echo "<input type='number' name='idnum' id='idnum' min='0'  />";
+	}
+	?>
     </td>
     <td>
     </td>
@@ -144,9 +253,10 @@ function cambiar(){
     <td>
     </td>
     <td>
+    <input type="submit" name="enviar" id="enviar" value="Enviar consulta" />
     </td>
     <td>
-    <input type="submit" name="enviar" id="enviar" value="Enviar consulta" />
+    <input type="reset" id="reset" value="Limpiar formulario" />
     </td>
     <td>
     </td>
@@ -158,25 +268,6 @@ function cambiar(){
 <br>
 
 <?php
-//Recibir
-$idnum = strip_tags($_POST['idnum']);
-$typeimg = strip_tags($_POST['typeimg']);
-$nombre_obj= strip_tags($_POST['nombre_obj']);
-$fecha_obs1= strip_tags($_POST['fecha_obs1']);
-$fecha_obs2= strip_tags($_POST['fecha_obs2']);
-// $tiempo_obs1= strip_tags($_POST['tiempo_obs1']);
-// $tiempo_obs2= strip_tags($_POST['tiempo_obs2']);
-$exptime1= strip_tags($_POST['exptime1']);
-$exptime2= strip_tags($_POST['exptime2']);
-$observatorio= strip_tags($_POST['observatorio']);
-$telescopio= strip_tags($_POST['telescopio']);
-$instrumento= strip_tags($_POST['instrumento']);
-$filtro= strip_tags($_POST['filtro']);
-
-
-
-
-
 
 
 $prefijo = "SELECT id, object, telescope, instrument, dateobs, timeobs, filter, imgtype, exptime, observatory, rute FROM tablaobs";
@@ -224,22 +315,22 @@ $sufijo = $sufijo . sprintf(" filter like '%%%s%%'",$filtro);
 if ((strlen($idnum) != 0) || (strlen($typeimg) != 0) || (strlen($nombre_obj) != 0) || (strlen($fecha_obs1) != 0) || (strlen($fecha_obs2) != 0) ||(strlen($exptime1) != 0) ||(strlen($exptime2) != 0) || (strlen($observatorio) != 0) || (strlen($telescopio) != 0) || (strlen($instrumento) != 0) || (strlen($filtro) != 0)) {
   $montamos = $prefijo . " WHERE" . $sufijo;
   $peticion = preg_replace('/and$/', '', $montamos);
-  echo "<table width='500' align='left'><tr><td>Se muestra la siguiente petición:</td><td> </td></tr>";
-  echo "<tr><td>Nombre del objeto:</td><td>" . $nombre_obj . "</td></tr>";
-  echo "<tr><td>Telescopio:</td><td>" . $telescopio . "</td></tr>";
-  echo "<tr><td>Instrumento:</td><td>" . $instrumento . "</td></tr>";
-  echo "<tr><td>Fecha de observación entre:</td><td>" . $fecha_obs1 . " y " . $fecha_obs2 . "</td></tr>";
-  echo "<tr><td>Filtro:</td><td>" . $filtro . "</td></tr>";
-  echo "<tr><td>Tipo de de imagen:</td><td>" . $typeimg . "</td></tr>";
-  echo "<tr><td>Tiempo de exposición entre:</td><td>" . $exptime1 . " y " . $exptime2 . "</td></tr>";
-  echo "<tr><td>Observatorio:</td><td>" . $observatorio . "</td></tr>";
-  echo "<tr><td>ID:</td><td>" . $idnum . "</td></tr>";
-  echo "</table>";
-  echo "<br>";
+//   echo "<table width='500' align='left'><tr><td>Se muestra la siguiente petición:</td><td> </td></tr>";
+//   echo "<tr><td>Nombre del objeto:</td><td>" . $nombre_obj . "</td></tr>";
+//   echo "<tr><td>Telescopio:</td><td>" . $telescopio . "</td></tr>";
+//   echo "<tr><td>Instrumento:</td><td>" . $instrumento . "</td></tr>";
+//   echo "<tr><td>Fecha de observación entre:</td><td>" . $fecha_obs1 . " y " . $fecha_obs2 . "</td></tr>";
+//   echo "<tr><td>Filtro:</td><td>" . $filtro . "</td></tr>";
+//   echo "<tr><td>Tipo de de imagen:</td><td>" . $typeimg . "</td></tr>";
+//   echo "<tr><td>Tiempo de exposición entre:</td><td>" . $exptime1 . " y " . $exptime2 . "</td></tr>";
+//   echo "<tr><td>Observatorio:</td><td>" . $observatorio . "</td></tr>";
+//   echo "<tr><td>ID:</td><td>" . $idnum . "</td></tr>";
+//   echo "</table>";
+//   echo "<br>";
   }
 else {
   $peticion = "SELECT id, object, telescope, instrument, dateobs, timeobs, filter, imgtype, exptime, observatory, rute FROM tablaobs WHERE DATE_SUB(CURDATE(), INTERVAL 31 DAY) <= dateobs ORDER BY dateobs DESC";
-  echo "<p>Se muestran las observaciones realizadas en los últimos 31 días.</p>";
+//   echo "<p>Se muestran las observaciones realizadas en los últimos 31 días.</p>";
   }
 
   
@@ -288,7 +379,7 @@ $archivos = array();
 	T. Exp.
       </th>
       <th width=14%>
-	Obs.
+	Observatorio
       </th>
       <th width=35%>
 	Ruta
